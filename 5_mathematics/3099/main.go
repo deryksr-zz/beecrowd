dbe877e86a22c75eec152041d7c01bf6df4513d8
Deryk Sedlak <deryk.sribeiro@gmail.com>
2021-09-07 16:19:27 -0300

    trying to resolve digit dp problem

--- 5_mathematics/3099/main.go
+++ 5_mathematics/3099/main.go
@@ -2,18 +2,53 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 )
 
-func main() {
-	var leitura int
-	fmt.Scanf("%d", &leitura)
-	var result int
-	var analyser string
-	for index := 1; index <= leitura; index++ {
-		analyser = strconv.Itoa(index)
-		result += strings.Count(analyser, "1") + strings.Count(analyser, "7")
+func getNewTight(value int, current int) int {
+	if value == current {
+		return 1
 	}
-	fmt.Printf("%d\n", result)
+	return 0
+}
+
+func getAnswer(index int, tight int, digits []int) int {
+	var limit int
+	result := 0
+
+	if index < 0 {
+		return 0
+	}
+
+	if tight == 0 {
+		limit = digits[index]
+	} else {
+		limit = 9
+	}
+
+	for i := 0; i < limit; i++ {
+
+		result += getAnswer(index-1, i, digits)
+	}
+
+	return result
+}
+
+func strToInt(text string) (result []int) {
+	array := strings.Split(text, "")
+	for _, value := range array {
+		conv := int(value[0] - '0')
+		result = append(result, conv)
+	}
+	return result
+}
+
+func main() {
+	var input string
+	fmt.Scanf("%s", &input)
+
+	digits := strToInt(input)
+	answer := getAnswer(len(digits)-1, 0, digits)
+
+	fmt.Println(answer)
 }
